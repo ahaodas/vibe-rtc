c01fde32da62c70fcd3c128d8b94e10e19ceed60
senyakrv <karavaev@skbkontur.ru>
2026-02-16 21:12:34 +0900

    chore(format): apply biome style in sdk package

--- packages/rtc-sdk/src/index.ts
+++ packages/rtc-sdk/src/index.ts
@@ -1,7 +1,6 @@
 export * from '@vibe-rtc/rtc-core'
-export * from '@vibe-rtc/rtc-react'
-export * from '@vibe-rtc/rtc-firebase'
-
 export * as rtcCore from '@vibe-rtc/rtc-core'
-export * as rtcReact from '@vibe-rtc/rtc-react'
+export * from '@vibe-rtc/rtc-firebase'
 export * as rtcFirebase from '@vibe-rtc/rtc-firebase'
+export * from '@vibe-rtc/rtc-react'
+export * as rtcReact from '@vibe-rtc/rtc-react'
